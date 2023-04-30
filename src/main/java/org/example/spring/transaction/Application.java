cbd505004b20ad7dc17c0f9f60cf663934d652b5
mia-developer <office.yeon@gmail.com>
2023-04-30 16:36:44 +0900

    feat: spring application

---
+++ src/main/java/org/example/spring/transaction/Application.java
@@ -0,0 +1,11 @@
+package org.example.spring.transaction;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Application.class, args);
+	}
+}
